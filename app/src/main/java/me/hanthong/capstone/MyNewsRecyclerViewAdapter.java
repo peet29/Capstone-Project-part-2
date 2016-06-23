a7c03fe1b902b0c66918b6ffc5684f8163e62533
Thanawat Hanthong <peet6015@gmail.com>
2016-06-23 21:26:12 +0700

    put news on the list

---
+++ app/src/main/java/me/hanthong/capstone/MyNewsRecyclerViewAdapter.java
@@ -0,0 +1,112 @@
+package me.hanthong.capstone;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import me.hanthong.capstone.data.NewsColumns;
+
+
+public class MyNewsRecyclerViewAdapter extends RecyclerView.Adapter<MyNewsRecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerViewAdapter";
+    private Cursor mCursor;
+    private Context mContext;
+
+    // BEGIN_INCLUDE(recyclerViewSampleViewHolder)
+    /**
+     * Provide a reference to the type of views that you are using (custom ViewHolder)
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView newsTitle;
+        private final TextView newsDate;
+        private final ImageView newsImage;
+
+        public ViewHolder(View v) {
+            super(v);
+            // Define click listener for the ViewHolder's View.
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "Element " + getPosition() + " clicked.");
+                }
+            });
+            newsTitle = (TextView) v.findViewById(R.id.news_title);
+            newsDate = (TextView) v.findViewById(R.id.news_date);
+            newsImage = (ImageView) v.findViewById(R.id.news_image);
+        }
+
+        public TextView getNewsTitle() {
+            return newsTitle;
+        }
+
+        public TextView getNewsDate() {
+            return newsDate;
+        }
+
+        public ImageView getNewsImage() {
+            return newsImage;
+        }
+    }
+    // END_INCLUDE(recyclerViewSampleViewHolder)
+
+
+    public MyNewsRecyclerViewAdapter(Context context) {
+        mContext = context;
+    }
+
+    // BEGIN_INCLUDE(recyclerViewOnCreateViewHolder)
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view.
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.fragment_news, viewGroup, false);
+
+        v.setFocusable(true);
+        return new ViewHolder(v);
+    }
+    // END_INCLUDE(recyclerViewOnCreateViewHolder)
+
+    // BEGIN_INCLUDE(recyclerViewOnBindViewHolder)
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        Log.d(TAG, "Element " + position + " set.");
+        mCursor.moveToPosition(position);
+        // Get element from your dataset at this position and replace the contents of the view
+        // with that element
+        viewHolder.getNewsTitle().setText(mCursor.getString(mCursor.getColumnIndex(NewsColumns.TITLE)));
+        viewHolder.getNewsDate().setText(mCursor.getString(mCursor.getColumnIndex(NewsColumns.DATE)));
+        Glide.with(mContext)
+                .load(mCursor.getString(mCursor.getColumnIndex(NewsColumns.PHOTO)))
+                .error(R.drawable.hold)
+                .crossFade()
+                .centerCrop()
+                .into(viewHolder.getNewsImage());
+
+    }
+    // END_INCLUDE(recyclerViewOnBindViewHolder)
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mCursor == null ? 0 : mCursor.getCount();
+    }
+
+    public Cursor getCursor(){return mCursor;}
+
+    public void swapCursor(Cursor newCursor) {
+        mCursor = newCursor;
+        notifyDataSetChanged();
+        //mEmptyView.setVisibility(getItemCount() == 0 ? View.VISIBLE : View.GONE);
+    }
+}
---
+++ app/src/main/java/me/hanthong/capstone/NewsFragment.java
@@ -0,0 +1,85 @@
+package me.hanthong.capstone;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.hanthong.capstone.data.NewsColumns;
+import me.hanthong.capstone.data.NewsProvider;
+
+
+public class NewsFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+
+    private static final int NEWS_LOADER = 0;
+    protected RecyclerView mRecyclerView;
+    protected MyNewsRecyclerViewAdapter mAdapter;
+    protected RecyclerView.LayoutManager mLayoutManager;
+
+    String[] PROJECTION = {
+            NewsColumns._ID,
+            NewsColumns.TITLE,
+            NewsColumns.DATE,
+            NewsColumns.LINK,
+            NewsColumns.PHOTO,
+            NewsColumns.FAV
+    };
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public NewsFragment() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getLoaderManager().initLoader(NEWS_LOADER, null, this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_news_list, container, false);
+
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.RecyclerView);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mAdapter = new MyNewsRecyclerViewAdapter(getActivity());
+        mRecyclerView.setAdapter(mAdapter);
+        return view;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String order  = NewsColumns._ID+" ASC";
+        return new CursorLoader(getActivity(), NewsProvider.Lists.LISTS,PROJECTION,null,null,order);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
+}
