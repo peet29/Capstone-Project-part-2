62be62209569cfc37c5de3146ed4e0e31ced56b1
Thanawat Hanthong <peet6015@gmail.com>
2016-06-25 18:25:59 +0700

    add tool bar to ReadListsFragment.java

--- app/src/main/java/me/hanthong/capstone/ReadLists.java
+++ app/src/main/java/me/hanthong/capstone/ReadLists.java
@@ -2,7 +2,6 @@ package me.hanthong.capstone;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class ReadLists extends AppCompatActivity {
 
@@ -10,9 +9,6 @@ public class ReadLists extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_lists);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
--- app/src/main/java/me/hanthong/capstone/ReadListsFragment.java
+++ app/src/main/java/me/hanthong/capstone/ReadListsFragment.java
@@ -1,9 +1,16 @@
 package me.hanthong.capstone;
 
+import android.app.LoaderManager;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +18,7 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class ReadListsFragment extends Fragment {
+public class ReadListsFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     public ReadListsFragment() {
     }
@@ -19,7 +26,14 @@ public class ReadListsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_read_lists, container, false);
+        View view = inflater.inflate(R.layout.fragment_read_lists, container, false);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        setHasOptionsMenu(true);
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        return view;
     }
 
     @Override
@@ -32,6 +46,28 @@ public class ReadListsFragment extends Fragment {
             return true;
         }
         return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // TODO Add your menu entries here
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_read_lists, menu);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
 
     }
 }
